2c6dae001f3bd68a60907dbbab2d99506d764d6f
inbox256 <inbox256@gmail.com>
2015-11-24 21:53:36 -0500

    other general methods

--- global_methods.rb
+++ global_methods.rb
@@ -6,6 +6,36 @@ def log(msg=nil, level='debug')
   Rails.logger.__send__ level, "#{File.basename(c.path)}->#{c.label}: #{msg}"
 end
 
+# Check if it is a number
+#
+def integer?(n)
+  n.to_i.to_s.eql?(n)
+end
+
+#
+# Copy nested hashes of any depth to a single delimited notation hash
+# Works for both regular and mongo hashes.
+#
+def notate_nested_hash(orighash, newhash, parent=nil)
+  orighash.each_key do |k|
+    dottedkey = (parent.nil?) ? k : "#{parent}=>#{k}"
+    if orighash[k].kind_of?(Hash) # instance_of? wouldn't work for mongo
+      notate_nested_hash(orighash[k], newhash, dottedkey) # recurse
+    else
+      newhash[dottedkey] = orighash[k]
+    end
+  end
+end
+
+
+# In an email, this format will allow both android and iphone to "add to calendar"
+#
+def calendarable_time_string(time)
+  d = time.strftime("%A %B %d, %Y") rescue ''
+  t = time.strftime("%l:%M %p") rescue ''
+  "#{d} at #{t}"
+end
+
 # Get the TZInfo::Timezone based on the zone identifier
 #
 def get_timezone(zone_identifier)
